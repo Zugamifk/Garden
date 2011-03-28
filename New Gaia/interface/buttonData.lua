4dd580ed4e9921269fd44eb31cf8ea79d56431f4
Patrick Berrigan <zugamifk@gmail.com>
2011-03-28 01:09:48 -0300

    Forgot the new files

---
+++ New Gaia/interface/buttonData.lua
@@ -0,0 +1,12 @@
+button.data = {
+	quitGame = {
+	    title = "Leave",
+	    fSize = 12,
+	    action = 0990000,
+	    
+		drawType = "colour",
+		BGcolour = C:colours("w"),
+		Ecolour = C:colours("b"),
+		Tcolour = C:colours("b")
+	}
+}
---
+++ New Gaia/interface/buttons.lua
@@ -0,0 +1,25 @@
+button = {}
+
+setmetatable(button, {__index = Window})
+
+function button:new(Window, version)
+	local newButton = {}
+	setmetatable(newButton, {__index = self.data[version.name]})
+	newButton.x = version.x
+	newButton.y = version.y
+	newButton.w = version.w
+	newButton.h = version.h
+	newButton.tx = version.tx
+	newButton.ty = version.ty
+	return newButton
+end
+
+function button:draw(b)
+	love.graphics.setColor(unpack(b.BGcolour))
+	love.graphics.rectangle("fill", b.x, b.y, b.w, b.h)
+	love.graphics.setColor(unpack(b.Ecolour))
+	love.graphics.rectangle("line", b.x, b.y, b.w, b.h)
+	love.graphics.setColor(unpack(b.Tcolour))
+	love.graphics.setFont(b.fSize)
+	love.graphics.print(b.title, b.x + b.tx, b.y + b.ty)
+end
